265f01578909d730921489e6a86f9a984223c68f
Deveshb15 <deveshb583@gmail.com>
2024-08-19 17:33:08 +0530

    changed api url

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 export const COMPUTER_IP_ADDRESS = "192.168.0.100";
 
-export const API_URL = `https://cozycast-backend-dev.vercel.app`;
+export const API_URL = `https://cozycast-backend.vercel.app`;
